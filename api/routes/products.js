c7f62ab110d8dba671d12e819859cedb780535f0
hnv1212 <h19982312@gmail.com>
2021-08-02 22:24:32 +0700

    first commit

---
+++ api/routes/products.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const router = express.Router();
+
+const multer = require("multer");
+const checkAuth = require('../middleware/check-auth');
+
+const ProductsController = require('../controllers/products');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().toISOString() + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5,
+  },
+  fileFilter: fileFilter
+});
+
+router.get("/", ProductsController.get_all_products);
+
+router.post("/", checkAuth, upload.single("productImage"), ProductsController.create_product);
+
+router.get("/:productId", ProductsController.get_product);
+
+router.patch("/:productId", checkAuth, ProductsController.update_product);
+
+router.delete("/:productId", checkAuth, ProductsController.delete_product);
+
+module.exports = router;
